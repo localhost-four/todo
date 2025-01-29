e12041ac7851d69f64c5b8e0c9e5a5eb5c6388c3
staffuser <119116574+staffuser@users.noreply.github.com>
2025-01-30 02:48:33 +0300

    Update script.js

--- forum/script.js
+++ forum/script.js
@@ -4,13 +4,13 @@ import { getFirestore, collection, addDoc, getDocs, query, where, Timestamp, upd
 // Firebase configuration
 const firebaseConfig = {
     // Your Firebase config here
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_mean_ID
+	apiKey: "${{ secrets.REACT_APP_API_KEY }}",
+	authDomain: "${{ secrets.REACT_APP_AUTH_DOMAIN }}",
+	projectId: "${{ secrets.REACT_APP_PROJECT_ID }}",
+	storageBucket: "${{ secrets.REACT_APP_STORAGE_BUCKET }}",
+	messagingSenderId: "${{ secrets.REACT_APP_MESSAGING_SENDER_ID }}",
+	appId: "${{ secrets.REACT_APP_APP_ID }}",
+	measurementId: "${{ secrets.REACT_APP_MEASUREMENT_ID }}"
 };
 
 const app = initializeApp(firebaseConfig);
