eed3c884c33cc4f6fee154dbe9aa143347970e87
staffuser <119116574+staffuser@users.noreply.github.com>
2025-01-30 02:46:12 +0300

    Update join.js

--- join.js
+++ join.js
@@ -13,13 +13,13 @@ const firebaseConfig = {
 	appId: process.env.REACT_APP_APP_ID,
         measurementId: process.env.REACT_APP_mean_ID
 	*/
-	REACT_APP_API_KEY: "${{ secrets.REACT_APP_API_KEY }}",
-	REACT_APP_AUTH_DOMAIN: "${{ secrets.REACT_APP_AUTH_DOMAIN }}",
-	REACT_APP_PROJECT_ID: "${{ secrets.REACT_APP_PROJECT_ID }}",
-	REACT_APP_STORAGE_BUCKET: "${{ secrets.REACT_APP_STORAGE_BUCKET }}",
-	REACT_APP_MESSAGING_SENDER_ID: "${{ secrets.REACT_APP_MESSAGING_SENDER_ID }}",
-	REACT_APP_APP_ID: "${{ secrets.REACT_APP_APP_ID }}",
-	REACT_APP_MEASUREMENT_ID: "${{ secrets.REACT_APP_MEASUREMENT_ID }}"
+	apiKey: "${{ secrets.REACT_APP_API_KEY }}",
+	authDomain: "${{ secrets.REACT_APP_AUTH_DOMAIN }}",
+	projectId: "${{ secrets.REACT_APP_PROJECT_ID }}",
+	storageBucket: "${{ secrets.REACT_APP_STORAGE_BUCKET }}",
+	messagingSenderId: "${{ secrets.REACT_APP_MESSAGING_SENDER_ID }}",
+	appId: "${{ secrets.REACT_APP_APP_ID }}",
+	measurementId: "${{ secrets.REACT_APP_MEASUREMENT_ID }}"
 };
 
 // Инициализация Firebase
