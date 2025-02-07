878855b825ab1c2123887164be50fb1284a4dd4f
staffuser <119116574+staffuser@users.noreply.github.com>
2025-02-08 00:48:31 +0300

    Update join.js

--- join.js
+++ join.js
@@ -2,6 +2,24 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js';
 import { getFirestore, collection, addDoc, getDocs, query, orderBy, limit, deleteDoc, doc } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js';
 
+/*
+apiKey: process.env.REACT_APP_API_KEY,
+authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+projectId: process.env.REACT_APP_PROJECT_ID,
+storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+appId: process.env.REACT_APP_APP_ID,
+measurementId: process.env.REACT_APP_mean_ID
+
+apiKey: secrets.REACT_APP_API_KEY,
+authDomain: secrets.REACT_APP_AUTH_DOMAIN,
+projectId: secrets.REACT_APP_PROJECT_ID,
+storageBucket: secrets.REACT_APP_STORAGE_BUCKET,
+messagingSenderId: secrets.REACT_APP_MESSAGING_SENDER_ID,
+appId: secrets.REACT_APP_APP_ID,
+measurementId: secrets.REACT_APP_MEASUREMENT_ID
+*/
+
 // Настройки Firebase
 const firebaseConfig = {
 	apiKey: window.REACT_APP_API_KEY,
@@ -11,23 +29,6 @@ const firebaseConfig = {
 	messagingSenderId: window.REACT_APP_MESSAGING_SENDER_ID,
 	appId: window.REACT_APP_APP_ID,
 	measurementId: window.REACT_APP_MEASUREMENT_ID
-	/*
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID,
-        measurementId: process.env.REACT_APP_mean_ID
-	
-	apiKey: secrets.REACT_APP_API_KEY,
-	authDomain: secrets.REACT_APP_AUTH_DOMAIN,
-	projectId: secrets.REACT_APP_PROJECT_ID,
-	storageBucket: secrets.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: secrets.REACT_APP_MESSAGING_SENDER_ID,
-	appId: secrets.REACT_APP_APP_ID,
-	measurementId: secrets.REACT_APP_MEASUREMENT_ID
-	*/
 };
 
 // Инициализация Firebase
