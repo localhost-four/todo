252c0a7314b52db0e82a79175a58b6278a51b9cf
staffuser <119116574+staffuser@users.noreply.github.com>
2025-02-09 18:53:06 +0300

    Update join.js

--- join.js
+++ join.js
@@ -2,33 +2,16 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js';
 import { getFirestore, collection, addDoc, getDocs, query, orderBy, limit, deleteDoc, doc } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js';
 
-/*
-apiKey: process.env.REACT_APP_API_KEY,
-authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-projectId: process.env.REACT_APP_PROJECT_ID,
-storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-appId: process.env.REACT_APP_APP_ID,
-measurementId: process.env.REACT_APP_mean_ID
-
-apiKey: secrets.REACT_APP_API_KEY,
-authDomain: secrets.REACT_APP_AUTH_DOMAIN,
-projectId: secrets.REACT_APP_PROJECT_ID,
-storageBucket: secrets.REACT_APP_STORAGE_BUCKET,
-messagingSenderId: secrets.REACT_APP_MESSAGING_SENDER_ID,
-appId: secrets.REACT_APP_APP_ID,
-measurementId: secrets.REACT_APP_MEASUREMENT_ID
-*/
 
 // Настройки Firebase
 const firebaseConfig = {
-	apiKey: window.REACT_APP_API_KEY,
-	authDomain: window.REACT_APP_AUTH_DOMAIN,
-	projectId: window.REACT_APP_PROJECT_ID,
-	storageBucket: window.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: window.REACT_APP_MESSAGING_SENDER_ID,
-	appId: window.REACT_APP_APP_ID,
-	measurementId: window.REACT_APP_MEASUREMENT_ID
+	apiKey: process.env.REACT_APP_API_KEY || secrets.REACT_APP_API_KEY || env.REACT_APP_MEASUREMENT_ID || window.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN || secrets.REACT_APP_AUTH_DOMAIN || env.REACT_APP_MEASUREMENT_ID || window.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID || secrets.REACT_APP_PROJECT_ID || env.REACT_APP_MEASUREMENT_ID || window.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET || secrets.REACT_APP_STORAGE_BUCKET || env.REACT_APP_MEASUREMENT_ID || window.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID || secrets.REACT_APP_MESSAGING_SENDER_ID || env.REACT_APP_MEASUREMENT_ID || window.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID || secrets.REACT_APP_APP_ID || env.REACT_APP_MEASUREMENT_ID || window.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_mean_ID || secrets.REACT_APP_MEASUREMENT_ID || env.REACT_APP_MEASUREMENT_ID || window.REACT_APP_MEASUREMENT_ID
 };
 
 // Инициализация Firebase
