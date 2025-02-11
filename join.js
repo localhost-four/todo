2ad71fe7dd1af059654a3ca208ff42641aa36bd1
staffuser <119116574+staffuser@users.noreply.github.com>
2025-02-11 07:20:08 +0300

    Update join.js

--- join.js
+++ join.js
@@ -154,16 +154,13 @@ async function createEdit() {
     editableField.contentEditable = true;
     editableField.innerText = title; // Устанавливаем title в поле
     editableField.style.position = 'absolute';
-    editableField.style.top = '2px';
-    editableField.style.left = '10px';
+    editableField.style.top = '1px';
+    editableField.style.left = '1px';
     editableField.style.zIndex = '1000';
-    editableField.style.padding = '5px';  // Уменьшаем отступы
-    editableField.style.fontSize = '14px';  // Меньше размер шрифта
+    editableField.style.padding = '1px';  // Уменьшаем отступы
     editableField.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
     editableField.style.border = '1px solid #ccc';  // Тонкая граница
     editableField.style.borderRadius = '3px';  // Более округлый угол
-    editableField.style.minWidth = '200px';  // Устанавливаем минимальную ширину
-    editableField.style.maxWidth = '300px';  // Ограничиваем максимальную ширину
     editableField.style.whiteSpace = 'nowrap'; // Убираем перенос строк
     
     // Добавляем поле на страницу
